23e7ebc97ca3418ea621bfa22d54d0cd940b4dec
Simon Côté <simon.cote@tlmgo.com>
2020-10-10 12:11:37 -0400

    Added game canvas.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const helloWorld: string = 'Hello world!'
+import 'phaser'
 
-alert(helloWorld)
+export default class JunglePlanetScene extends Phaser.Scene {
+  constructor() {
+    super('demo')
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#125444',
+  width: 960,
+  height: 640,
+  scene: JunglePlanetScene
+}
+
+new Phaser.Game(config)
